908a0a23d9d9de7b678d812780f278849aa8663c
Wazir123456 <158404389+Wazir123456@users.noreply.github.com>
2025-12-11 00:57:19 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your hobby: ")
+
+print("\n Welcome Message ")
+print(f"Hello everyone . My name is {name}.I am {age} years old and I love {hobby}.")
+print("Nice to meet you all! 😊")
